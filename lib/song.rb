b6aa06af9720adc0ec0caf667db0927f18ba6039
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 13:44:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,8 @@
 class Song
-
-  @@all = []
-
   attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def artist_name
@@ -20,5 +12,4 @@ class Song
       @artist.name
     end
   end
-end
 end
