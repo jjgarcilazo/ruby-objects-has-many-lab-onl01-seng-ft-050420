19081797c467161f1c1daabae7ae9cc18d64119c
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 13:22:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,7 @@ class Artist
    end
 
  def songs
-  Song.all.select { |song| song.artist == adele } 
+  Song.all.select { |song| song.artist == adele }
  #since song belongs to artist, we can call song.artist and it shoudl return the instnace of the artist that it's associated with
  end
 
