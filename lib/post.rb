9733b45e7c8b743805c9a849310e988a8804c8b8
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 13:45:03 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,15 @@
 class Post
-  attr_accessor :author, :title
 
+  attr_accessor :title, :author
   def initialize(title)
     @title = title
   end
 
   def author_name
-    self.author ? self.author.name : nil
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
   end
-end 
+end
