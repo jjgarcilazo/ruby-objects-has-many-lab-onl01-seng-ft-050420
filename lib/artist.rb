27cac74e605b9da3bd36f18467354f4da9abead3
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 12:58:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  @@song_count = 3
+
+  def initialize(name)
+    @name = name
+    @songs = "Hello"
+  end
+
+  def self.song_count
+ Song.all.count
+end
+
+
+  def songs
+    @songs
+  end
+
+  def add_song(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+end
