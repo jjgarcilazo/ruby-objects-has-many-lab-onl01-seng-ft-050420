35ffa80921e12b1931c68201ed65ae588ce504b0
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-20 13:15:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,32 @@
-require 'pry'
-
-class Artist
-  attr_accessor :name
-
-  @@song_count = 3
-
+require "pry"
+class Artist 
+  attr_accessor :name, :song, :artist, :new_song
+ 
+ @@song_count = 0
+  
   def initialize(name)
     @name = name
-    @songs = "Hello"
-  end
-
-  def self.song_count
- Song.all.count
+   end
+ 
+ def songs
+  Song.all.select { |song| song.artist == self }
+ #since song belongs to artist, we can call song.artist and it shoudl return the instnace of the artist that it's associated with
+ end
+ 
+ def add_song_by_name(name)
+   new_song = Song.new(name) 
+   self.songs << new_song 
+   new_song.artist = self 
+   @@song_count += 1 
+ end
+ 
+  
+ def self.song_count
+  Song.all.count
+ end
+ 
+  def add_song(song)
+  song.artist = self
 end
 
-
-  def songs
-    @songs << song
-
-    binding.pry
-  end
-
-  def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
 end
